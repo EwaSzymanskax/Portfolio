103d0492cc498026ac5231f16f03c1ec810a5527
Ewa Szymańska <ewa.szymanskax@gmail.com>
2023-10-07 23:35:47 +0200

    add giardDesign  project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import style from "./Projects.module.css";
 import portfolioTemplate from '../image/portfolioTemplate.png';
-import ReflexGame from '../image/ReflexGame.png';
-import ListOfProducts from '../image/ListOfProducts.png';
-import ExpensesList from '../image/ExpensesList.png';
-import TicTacToe from '../image/TicTacToe.png';
+import reflexGame from '../image/ReflexGame.png';
+import listOfProducts from '../image/ListOfProducts.png';
+import expensesList from '../image/ExpensesList.png';
+import ticTacToe from '../image/TicTacToe.png';
+import giardDesign from '../image/GiardDesign.png';
 
 function Projects(){
     const [hover, setHover] = useState(false);
@@ -15,6 +16,15 @@ function Projects(){
     <h1 className={style.h1Project}>Projects:</h1>
     <div className={style.projectsBox} onMouseEnter={()=> setHover(true)}
         onMouseLeave={()=> setHover(false)}>
+        <div className={style.card} >
+            <div className={style.aboutProject}>
+                <h2  className={style.h2Projects}>GiardDesign</h2>
+                <p className={style.pProjects}>Website design with TypeScript.</p>
+            </div>
+            <a href="https://ewaszymanskax.github.io/GiardDesignWebsite/" >
+            <img className={style.imgProjects} src={giardDesign} alt="website"/> 
+            </a>
+        </div>
         <div className={style.card} >
             <div className={style.aboutProject}>
                 <h2  className={style.h2Projects}>Portfolio Template</h2>
@@ -30,7 +40,7 @@ function Projects(){
                 <p className={style.pProjects}>The game that a player has to click on a green randomly selected square.</p>
             </div>
             <a href="https://ewaszymanskax.github.io/ReflexGame/" >
-            <img className={style.imgProjects} src={ReflexGame} alt="game"/>
+            <img className={style.imgProjects} src={reflexGame} alt="game"/>
             </a>
         </div>
         <div  className={style.card}>
@@ -39,7 +49,7 @@ function Projects(){
                 <p className={style.pProjects}>The app returns search results that match the parameters specified in the API.</p>
                 </div>
                 <a href= "https://ewaszymanskax.github.io/ListOfProducts/" >
-                <img className={style.imgProjects} src={ListOfProducts} alt="app"/>
+                <img className={style.imgProjects} src={listOfProducts} alt="app"/>
                 </a>
         </div>
         <div  className={style.card}>
@@ -48,7 +58,7 @@ function Projects(){
                 <p className={style.pProjects}>The app includes list of expenses with the date, the amount and the expenses browser.</p>
                 </div>
                 <a href="https://ewaszymanskax.github.io/ExpensesList/" >
-                <img className={style.imgProjects} src={ExpensesList} alt="app"/>
+                <img className={style.imgProjects} src={expensesList} alt="app"/>
                 </a>
         </div>
             <div className={style.card}>
@@ -57,7 +67,7 @@ function Projects(){
                 <p className={style.pProjects}>The interactive tic-tac-toe game with React.</p>
                 </div>
                 <a href="https://ewaszymanskax.github.io/TicTacToeGame/" >
-                <img className={style.imgProjects} src={TicTacToe} alt="tic-tac-toe"/>
+                <img className={style.imgProjects} src={ticTacToe} alt="tic-tac-toe"/>
                 </a>
         </div>
     </div>
