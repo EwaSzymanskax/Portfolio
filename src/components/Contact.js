1f5ff70465213353fc5815a2b59fce8f19525b2b
Ewa Szymańska <ewa.szymanskax@gmail.com>
2023-11-21 23:17:28 +0100

    add LinkedIn

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,7 +10,7 @@ function Contact(){
             <h2 className={style.h2Contact}>+48 512-933-509</h2>
             <ul className={style.ulContact}>
                 <li className={style.li}><a href="https://github.com/EwaSzymanskax">GitHub</a></li>
-                <li className={style.li}><a href="/#">LinkedIn</a></li> 
+                <li className={style.li}><a href="https://linkedin.com/in/ewa-szymanskax">LinkedIn</a></li> 
             </ul>
             <button className={style.scroll}><a href="#home" className={style.aContact}>Scroll to top</a></button>
         </div>
